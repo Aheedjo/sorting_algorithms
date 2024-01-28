c78ba065932174d5aae24a22893a88c264959827
Aheedjo <maishanu.ahijo@yahoo.com>
2024-01-28 03:33:32 +0100

    Task 104

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,145 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
-/**
- * copy - copies data from one buffer to another
- *
- * @src: source buffer
- * @dst: destination buffer
- * @size: size of buffers
- *
- * Return: No Return
- */
-void copy(int *src, int *dst, int size)
-{
-	int i;
+void swap_ints(int *a, int *b);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
 
-	for (i = 0; i < size; i++)
-		dst[i] = src[i];
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
+
 /**
- * merge - merges two sets of data in ascending order
- * but they must already be sorted before hand
- * @array: first set of data
- * @buff: second set of data
- * @minL: lower range of first set of data
- * @maxL: upper range of first set of data
- * @minR: lower range of second set of data
- * @maxR: upper range of second set of data
- *
- * Return: No Return
+ * max_heapify - Turn a binary tree into a complete binary heap.
+ * @array: An array of integers representing a binary tree.
+ * @size: The size of the array/tree.
+ * @base: The index of the base row of the tree.
+ * @root: The root node of the binary tree.
  */
-void merge(int *array, int *buff, int minL, int maxL, int minR, int maxR)
+void max_heapify(int *array, size_t size, size_t base, size_t root)
 {
-	int i = minL, j = minR, k = minL;
+	size_t left, right, large;
 
-	while (i <= maxL || j <= maxR)
+	left = 2 * root + 1;
+	right = 2 * root + 2;
+	large = root;
 
-		if (i <= maxL && j <= maxR)
-			if (buff[i] <= buff[j])
-				array[k] = buff[i], k++, i++;
-			else
-				array[k] = buff[j], k++, j++;
+	if (left < base && array[left] > array[large])
+		large = left;
+	if (right < base && array[right] > array[large])
+		large = right;
 
-		else if (i > maxL && j <= maxR)
-			array[k] = buff[j], k++, j++;
-		else
-			array[k] = buff[i], k++, i++;
-}
-/**
- * printcheck - prints an array in a given range
- *
- * @array: array of data to be print
- * @r1: start of range
- * @r2: end of range
- *
- * Return: No Return
- */
-void printcheck(int *array, int r1, int r2)
-{
-	int i;
-
-	for (i = r1; i <= r2; i++)
+	if (large != root)
 	{
-		if (i > r1)
-			printf(", ");
-		printf("%d", array[i]);
+		swap_ints(array + root, array + large);
+		print_array(array, size);
+		max_heapify(array, size, base, large);
 	}
-	printf("\n");
 }
+
 /**
- * split - recursive function to split data into merge tree
+ * heap_sort - Sort an array of integers in ascending
+ *             order using the heap sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
- * @array: array of data to be split
- * @buff: auxiliary array of data for merging
- * @min: min range of data in array
- * @max: max range of data in array
- * @size: size of total data
- *
- * Return: No Return
+ * Description: Implements the sift-down heap sort
+ * algorithm. Prints the array after each swap.
  */
-void split(int *array, int *buff, int min, int max, int size)
+void heap_sort(int *array, size_t size)
 {
-	int mid, tmax, minL, maxL, minR, maxR;
+	int i;
 
-	if ((max - min) <= 0)
+	if (array == NULL || size < 2)
 		return;
 
-	mid = (max + min + 1) / 2;
-	tmax = max;
-	max = mid - 1;
+	for (i = (size / 2) - 1; i >= 0; i--)
+		max_heapify(array, size, size, i);
 
-	minL = min;
-	maxL = max;
-
-	split(array, buff, min, max, size);
-
-	min = mid;
-	max = tmax;
-
-	minR = min;
-	maxR = max;
-
-	split(array, buff, min, max, size);
-
-	printf("Merging...\n");
-	printf("[left]: ");
-
-	printcheck(array, minL, maxL);
-
-	printf("[right]: ");
-
-	printcheck(array, minR, maxR);
-	merge(array, buff, minL, maxL, minR, maxR);
-	copy(array, buff, size);
-
-	printf("[Done]: ");
-	printcheck(array, minL, maxR);
-}
-/**
- * merge_sort - sorts an array of integers in ascending order
- * using the Merge sort algorithm
- *
- * @array: array of data to be sorted
- * @size: size of data
- *
- * Return: No Return
- */
-void merge_sort(int *array, size_t size)
-{
-	int *buff;
-
-	if (size < 2)
-		return;
-
-	buff = malloc(sizeof(int) * size);
-	if (buff == NULL)
-		return;
-
-	copy(array, buff, size);
-
-	split(array, buff, 0, size - 1, size);
-
-	free(buff);
+	for (i = size - 1; i > 0; i--)
+	{
+		swap_ints(array, array + i);
+		print_array(array, size);
+		max_heapify(array, size, i, 0);
+	}
 }
